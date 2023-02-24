c70057a37956b217b20a2dd022b948dd1e5ee9cf
meaLuda <munyalamea@gmail.com>
2023-02-24 12:50:04 +0300

    added comments

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,6 +4,11 @@ require 'openssl'
 
 
 class GamesController < ApplicationController
+  """
+    # Get the standings from the API
+    # https://rapidapi.com/api-sports/api/api-football
+    # Show case the data in the view as stangins
+  """
     def index
       url = URI("https://premier-league-standings1.p.rapidapi.com/")
 
@@ -12,6 +17,8 @@ class GamesController < ApplicationController
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
   
       request = Net::HTTP::Get.new(url)
+
+      # this api key is for testing purposes only will be removed after the project is done
       request["X-RapidAPI-Key"] = 'e32ac445e6msh34a1aa4e3b8ab24p1ad76cjsn528d9475314b'
       request["X-RapidAPI-Host"] = 'premier-league-standings1.p.rapidapi.com'
       
--- app/controllers/standings_controller.rb
+++ app/controllers/standings_controller.rb
@@ -2,6 +2,7 @@
 
 # save liked game
 def Like
+    # load params from request into variables
     team_rank = params[:team_rank]
     team_name = params[:team_name]
     games_played = params[:games_played]
@@ -15,6 +16,7 @@ def Like
         return
     end
     
+    # Create new team stats from params--variables
     team_stats = TeamStats.new(rank: team_rank, name: team_name, games_played: games_played, wins: wins, losses: losses, ties: ties)
     puts "-------- team_stats saved --------"
     team_stats.save
